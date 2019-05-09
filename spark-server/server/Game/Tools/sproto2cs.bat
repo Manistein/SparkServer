aacce40a16dba1cffa646e6d1ac5f1dae857e905
manistein <1518986465@qq.com>
2019-05-09 20:09:20 +0800

    生成sproto cs文件的脚本共享一个库

--- spark-server/server/Game/Tools/sproto2cs.bat
+++ spark-server/server/Game/Tools/sproto2cs.bat
@@ -2,10 +2,10 @@
 @REM since:2019.04.26
 @REM desc:dump sproto 2 cs
 
-set RPCProtoCS=%~dp0RPCProtoCS\\
-set RPCProtoSchema=%~dp0RPCProtoSchema\\
+set RPCProtoCS=%~dp0..\\Resource\\RPCProtoCS\\
+set RPCProtoSchema=%~dp0..\\Resource\\RPCProtoSchema\\
 
-set WorkingPath=..\\..\\..\\Framework\\Tools\\sproto2cs\\lib\\
+set WorkingPath=..\\..\\Framework\\Tools\\sproto2cs\\lib\\
 pushd %WorkingPath%
 
 lua.exe ..\\sproto2cs.lua %RPCProtoSchema% %RPCProtoCS%
--- spark-server/server/Game/Tools/sprotodump/sproto2cs.bat
+++
@@ -1,7 +0,0 @@
-@REM author:ddb
-@REM since:2019.04.26
-@REM desc:dump sproto 2 cs
-
-..\\..\\..\\Framework\\Tools\\sproto2cs\\lib\\lua.exe sproto2cs.lua
-
-pause
